8bcda877acea08867d4b2fa06647df8402a0d7d8
Aditya Pandit <adityapandit38@gmail.com>
2025-07-16 18:43:39 +0530

    Added password authentication to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,21 @@
 class User < ApplicationRecord
-  has_secure_password
+  include BCrypt
+
+  # Validations
+  validates :email, presence: true, uniqueness: true
+  validates :password, length: { minimum: 6 }, allow_nil: true
+  validates :password, presence: true
+
+  def password
+    @password = Password.new(password_digest) if password_digest.present?
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password) if new_password.present?
+    self.password_digest = @password.to_s if @password
+  end
+
+  def authenticate(submitted_password)
+    password == submitted_password
+  end
 end
