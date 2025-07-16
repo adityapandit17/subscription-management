1755af1a130fce8086889906a74954d753fe06da
Aditya Pandit <adityapandit38@gmail.com>
2025-07-17 01:07:44 +0530

    routes cleanup and made them restfull

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
   root to: "subscriptions#index"
 
-  get "login", to: "sessions#new", as: :login
-  post "login", to: "sessions#create"
-  delete "logout", to: "sessions#destroy", as: :logout
-
-  get "subscriptions/index"
-  resources :subscriptions, only: :index
-
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  get "sessions/new", to: "sessions#new", as: :login
+  delete "sessions", to: "sessions#destroy", as: :logout
+  get "subscriptions/index"
+
   # for handling stripe webhooks events
   resources :stripe_webhooks, only: [ :create ]
+  resource :session, only: [:new, :create, :destroy]
+  resources :subscriptions, only: :index
 end
