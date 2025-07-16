b92acfa2120b1770304c8d51867f49763d50146e
Aditya Pandit <adityapandit38@gmail.com>
2025-07-17 01:59:27 +0530

    Added filters for subscriptions

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -5,6 +5,13 @@ class SubscriptionsController < ApplicationController
   before_action :require_login
 
   def index
-    @subscriptions = Subscription.includes(:user).page(params[:page]).per(5)
+    @subscriptions = Subscription.includes(:user)
+
+    if params[:status].present? && Subscription.statuses.key?(params[:status])
+      @subscriptions = @subscriptions.where(status: params[:status])
+    end
+
+    @subscriptions = @subscriptions.page(params[:page]).per(5)
+    @current_filter = params[:status]
   end
 end
